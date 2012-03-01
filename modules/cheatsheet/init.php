804422f65044b7692efda9a68e911f3fffc2e82d
Vitaly Kustov <ATzal1k4@mail.ru>
2012-03-01 11:48:39 +0700

    3 commit

--- modules/cheatsheet/init.php
+++
@@ -1,7 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.');
-
-Route::set('cheatsheet', 'cs')
-	->defaults(array(
-		'controller' => 'cheatsheet',
-		'action'     => 'index',
-	));
--- modules/email-queue/classes/emailqueue.php
+++
@@ -1,3 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.');
-
-class EmailQueue extends EmailQueue_Core {}
